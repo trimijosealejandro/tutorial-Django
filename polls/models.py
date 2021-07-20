2f384ad56d74fa674e7647e7c1e4565ebafa5836
trimijosealejandro93 <trimijosealejandro@yahoo.com>
2021-07-20 14:18:35 -0400

    Base de datos

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.db.models.fields import CharField, DateField
 
 class Question (models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-class Choice(models.Models):
+class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = CharField(max_length=100)
-    voice = models.IntegerField(default=0)
+    choice_text = models.CharField(max_length=100)
+    votes = models.IntegerField(default=0)
